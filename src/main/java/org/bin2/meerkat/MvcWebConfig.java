ad625e189a368ae8543da0f5e8df4d1bddfb4515
broger <benoit.roger@insparx.com>
2015-03-23 17:16:05 +0100

    add websocket

--- src/main/java/org/bin2/meerkat/MvcWebConfig.java
+++ src/main/java/org/bin2/meerkat/MvcWebConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @EnableWebMvc
+@EnableWebSocket
 @ComponentScan(basePackages = {
         "org.bin2.meerkat.action"
 })
